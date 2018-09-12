fcc035cda2a4a15695d79054c099339c7b7237b0
thiagogarbazza <thiagogarbazza@gmail.com>
2018-09-12 06:55:36 -0300

    remove invalid copyright add by intellij

--- runtime/src/main/java/com/github/thiagogarbazza/expressionresolve/compile/Compiler.java
+++ runtime/src/main/java/com/github/thiagogarbazza/expressionresolve/compile/Compiler.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) Banco Central do Brasil.
- *
- * Este software é confidencial e propriedade do Banco Central do Brasil.
- * Não é permitida sua distribuição ou divulgação do seu conteúdo sem
- * expressa autorização do Banco Central.
- * Este arquivo contém informações proprietárias.
- */
 package com.github.thiagogarbazza.expressionresolve.compile;
 
 import org.antlr.v4.runtime.tree.ParseTree;
