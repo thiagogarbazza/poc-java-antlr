37f27db804b4d3c450373f207931663d21892e9d
thiagogarbazza <thiagogarbazza@gmail.com>
2018-10-01 08:29:07 -0300

    simplify parseTreeBuilder

--- src/main/java/com/github/thiagogarbazza/expressionresolver/compile/Compiler.java
+++ src/main/java/com/github/thiagogarbazza/expressionresolver/compile/Compiler.java
@@ -5,17 +5,18 @@ import com.github.thiagogarbazza.expressionresolver.parser.ExpressionLexer;
 import com.github.thiagogarbazza.expressionresolver.parser.ExpressionParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import static com.github.thiagogarbazza.expressionresolver.compile.ParseTreeBuilder.getParseTreeBuilder;
+
 public final class Compiler {
 
   private final LexerBuilder lexerBuilder = new LexerBuilder();
   private final ParserBuilder parserBuilder = new ParserBuilder();
-  private final TreeBuilder treeBuilder = new TreeBuilder();
 
   public ParseTree compile(Expression expression) {
     ExpressionInputStream in = new ExpressionInputStream(expression);
     ExpressionLexer lexer = lexerBuilder.build(in);
     ExpressionParser parser = parserBuilder.build(lexer);
-    ParseTree tree = treeBuilder.build(parser);
-    return tree;
+
+    return getParseTreeBuilder().build(parser);
   }
 }
--- src/main/java/com/github/thiagogarbazza/expressionresolver/compile/ParseTreeBuilder.java
+++ src/main/java/com/github/thiagogarbazza/expressionresolver/compile/ParseTreeBuilder.java
@@ -1,38 +1,36 @@
 package com.github.thiagogarbazza.expressionresolver.compile;
 
 import com.github.thiagogarbazza.expressionresolver.parser.ExpressionParser;
+import lombok.NoArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
 import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import static lombok.AccessLevel.PRIVATE;
 import static org.antlr.v4.runtime.atn.PredictionMode.LL;
 import static org.antlr.v4.runtime.atn.PredictionMode.SLL;
 
 @CommonsLog
-final class TreeBuilder {
+@NoArgsConstructor(access = PRIVATE)
+final class ParseTreeBuilder {
+
+  private static final ParseTreeBuilder INSTANCE = new ParseTreeBuilder();
 
   public ParseTree build(final ExpressionParser parser) {
-    ParseTree tree;
     try {
-             /*
-             * Tenta inicialmente utilizar o modo de predição mais simples e rápido:
-             * o SLL.
-             */
-      tree = getParseTree(parser, SLL);
-            /*
-             * Se chegar até aqui, não há nenhum erro sintático e o modo SLL foi
-             * suficiente, não havendo necessidade de se tentar o LL(*).
-             */
+      return getParseTree(parser, SLL);
     } catch (Exception e) {
-            /* Tenta agora o modo de predição mais completo: o LL(*) */
-      tree = getParseTree(parser, LL);
+      return getParseTree(parser, LL);
     }
-    return tree;
   }
 
   private ParseTree getParseTree(final ExpressionParser parser, final PredictionMode predictionMode) {
     parser.reset();
     parser.getInterpreter().setPredictionMode(predictionMode);
     return parser.parse();
+  }
+
+  public static ParseTreeBuilder getParseTreeBuilder() {
+    return INSTANCE;
   }
 }
