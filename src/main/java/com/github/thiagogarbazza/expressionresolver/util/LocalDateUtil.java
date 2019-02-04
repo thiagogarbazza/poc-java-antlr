50daa681a410ba5572f4e513d1dbe3ea2d36e75d
Thiago Garbazza <thiagogarbazza@gmail.com>
2019-02-04 17:56:23 -0200

    fix code-smell.

--- src/main/java/com/github/thiagogarbazza/expressionresolver/util/LocalDateUtil.java
+++ src/main/java/com/github/thiagogarbazza/expressionresolver/util/LocalDateUtil.java
@@ -1,8 +1,11 @@
 package com.github.thiagogarbazza.expressionresolver.util;
 
+import lombok.experimental.UtilityClass;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public class LocalDateUtil {
 
   private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy/MM/dd");
--- src/test/java/com/github/thiagogarbazza/expressionresolver/util/LocalDateUtilTest.java
+++ src/test/java/com/github/thiagogarbazza/expressionresolver/util/LocalDateUtilTest.java
@@ -10,7 +10,13 @@ import static org.junit.Assert.assertEquals;
 public class LocalDateUtilTest {
 
   @Test
-  public void verifyStringToLocalDate() {
+  public void verifyToLocalDate() {
     assertEquals(LocalDate.of(2018, 12, 31), toLocalDate("2018/12/31"));
+  }
+
+
+  @Test
+  public void verifyToLocalDateLeapYear() {
+    assertEquals(LocalDate.of(2016, 02, 29), toLocalDate("2016/02/29"));
   }
 }
