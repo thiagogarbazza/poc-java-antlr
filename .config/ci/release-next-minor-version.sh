74a6c8d4d47ac65ea88cdaa12ab73eb523ba0a0a
Thiago Garbazza <1093328+thiagogarbazza@users.noreply.github.com>
2025-06-04 17:59:50 -0300

    feat: add script and workflow for preparing next minor version release

---
+++ .config/ci/release-next-minor-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+current_version=$(sed -ne '5,10 s/<version>\(.*\)<\/version>/\1/p' pom.xml | tr -d '[:space:]')
+major_value=$(echo "$current_version" | cut -d. -f1)
+minor_value=$(echo "$current_version" | cut -d. -f2)
+next_version="$major_value.$(($minor_value+1)).0-SNAPSHOT"
+
+echo -e "
+  current_version: $current_version
+  next_version: $next_version
+"
+
+./mvnw \
+  --batch-mode \
+  -DdevelopmentVersion="$next_version" \
+  release:update-versions
+
+git commit --all --message "release: prepare next minor version iteration $next_version"
